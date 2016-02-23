86006b65b073fcea8b5ea0026eb588fd3e09ad4e
antoinfive <antoincampbell@live.com>
2016-02-23 12:17:23 -0500

    refactored and still able to make association, but for Janus page only. Next up: Grab session token and store it for each request across and across any browser

--- background.js
+++ background.js
@@ -1,35 +1,28 @@
-var signedIn;
-var currentUser;
-
-signedIn = function(auth_token){
-  if(auth_token==="undefined"){
-    return false;
-  }else {
-    return true;
-  }
-};
 
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var activeTab = tabs[0];
-      if(activeTab !== "undefined") {
-        chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-      } else {
-        alert("the things are breaking");
+    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"}, function(response){
+      var lastError = chrome.runtime.lastError;
+        if(lastError){
+          console.log(lastError.message);
+        } else {
+          console.log(response);
       }
+    });
   });
 });
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.action === "xhttp" ) {
-        var title = "bookmark from " + request.data;
+        var title = "Bookmark saved from JanusChrome: " + sender.url;
         var xhttp = new XMLHttpRequest();
         var method = "POST";
         xhttp.open(method, request.url, true);
         xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
         xhttp.send(JSON.stringify({bookmark:
-          {link: request.data,
+          {link: sender.url,
             title: title},
           user:{token: request.token}
         }));
@@ -40,21 +33,3 @@ chrome.runtime.onMessage.addListener(
     }
   }
 );
-
-//
-// chrome.runtime.onMessage.addListener(
-//   function(request, sender, sendResponse) {
-//     alert("eagle approaching");
-//     if( request.message === "sendZeTokens" ) {
-//         var xhttp = new XMLHttpRequest();
-//         xhttp.open(request.method, request.url, true);
-//         xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-//         xhttp.send(JSON.stringify({user: request.data}));
-//         alert('eagle landing');
-//         xhttp.onload = function(){
-//           callback(xhttp.responseText);
-//         };
-//         return true;
-//     }
-//   }
-// );
--- save.js
+++ save.js
@@ -2,21 +2,21 @@ chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
 
     if( request.message === "clicked_browser_action" ) {
-      var firstHref = $("a[href^='http']").eq(0).attr("href");
+      // var firstHref = $("a[href^='http']").eq(0).attr("href");
       var theGoods = localStorage.getItem('ember_simple_auth:session');
-      console.log(firstHref);
-      if(firstHref !== "undefined"){
+      // console.log(firstHref);
+      if(sender !== undefined){
       chrome.runtime.sendMessage({
                method: "POST",
                action: "xhttp",
                url: "http://localhost:3000/api/v1/bookmarks/chrome",
-               data: firstHref,
+               data: sender,
                token: theGoods.match(/(?:"token":")(\w+)(?:","email")/)[1]
            }, function(responseText) {
                console.log(responseText);
         });
       } else {
-        alert("NOPE");
+        alert("the url you tried to save was invalid, please try again.");
       }
     }
   }
